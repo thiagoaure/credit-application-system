7a6a5a1d3da11504da185e6c812dc0f0fd761cc2
Thiago Aureliano <thiagoh544@gmail.com>
2023-05-26 21:12:12 -0300

    feat(all): add camada de serviço

---
+++ src/main/kotlin/me/dio/credit/application/system/service/CreditService.kt
@@ -0,0 +1,30 @@
+package me.dio.credit.application.system.service
+
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.repository.ICreditRepository
+import java.lang.RuntimeException
+import java.util.*
+
+class CreditService(
+    private val creditRepository: ICreditRepository,
+    private val customerService: CustomerService
+): ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> =
+        this.creditRepository.findAllByCustomerId(customerId)
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
+            ?: throw RuntimeException("CreditCode $creditCode not found"))
+        if (credit.customer?.id == customerId) {
+          return credit
+        } else {
+          throw RuntimeException("Contact admin")
+        }
+    }
+}
